db2fef0ac4ec234d3bb4324f6a30d2b97405fded
hteamxapp <hteamxapp@gmail.com>
2025-05-18 12:11:22 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,13 @@
 import { auth } from './firebase.js';
 import {
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const loginBtn = document.getElementById("loginBtn");
+  const registerBtn = document.getElementById("registerBtn");
+
   if (loginBtn) {
     loginBtn.addEventListener("click", () => {
       const email = document.getElementById("email").value;
@@ -16,6 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(error => {
           alert("Sai tài khoản hoặc mật khẩu");
+        });
+    });
+  }
+
+  if (registerBtn) {
+    registerBtn.addEventListener("click", () => {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+          alert("Đăng ký thành công!");
+          window.location.href = "taikhoan.html";
+        })
+        .catch(error => {
+          alert("Đăng ký thất bại: " + error.message);
         });
     });
   }
